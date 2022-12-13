939ddd717acebaa3dbf1deef14e070b29d6f306a
Davron01~ <d.mirabdullayev@mail.ru>
2022-12-13 11:55:43 +0500

    project ends

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import Countries from "./components/Countries";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Error from "./components/Error";
 import SingleCountry from "./components/SingleCountry";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Countries/>}/>
-          <Route path="/:name" element={<SingleCountry/>}/>
-          <Route path="*" element={<Error/>}/>
+          <Route path="/" element={<Countries />} />
+          <Route path="/:name" element={<SingleCountry />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,22 +1,75 @@
-import { async } from "q";
 import React, { useState, useEffect } from "react";
 import Article from "./Article";
 
 const Countries = () => {
+  // 1 01 36
   const [countries, setCountries] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const regions = [
+    {
+      name: "Europe",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Africa",
+    },
+    {
+      name: "America",
+    },
+    {
+      name: "Antarctic",
+    },
+  ];
+  useEffect(() => {
+    document.title = `Showin All Countries `;
+  }, []);
 
   useEffect(() => {
     const getCountries = async () => {
       try {
         const res = await fetch("https://restcountries.com/v3.1/all");
         const data = await res.json();
-        setCountries(data.slice(0,10));
+        setCountries(data);
       } catch (error) {
         console.error(error);
       }
     };
     getCountries();
   }, []);
+
+  async function searchCountry() {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${searchText}`
+      );
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async function filterByRegion(region) {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/region/${region}`
+      );
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleSearchCountry(e) {
+    e.preventDefault();
+    searchCountry();
+  }
+  function handleFilterByRegion(e) {
+    e.preventDefault();
+    filterByRegion();
+  }
   return (
     <>
       {!countries ? (
@@ -25,11 +78,54 @@ const Countries = () => {
         </h1>
       ) : (
         <section className="container mx-auto p-8">
-          <div className="grid cols-1 gap-8 md:grid-cols-2
-          lg:grid-cols-3 2xl:grid-cols-4">
-          {countries.map((country) => (
-            <Article key={country.name.common} {...country} />
-          ))}
+          <div
+            className="flex flex-col gap-4 md:flex-row
+          md:items-center md:justify-between mb-7"
+          >
+            <form
+              onSubmit={handleSearchCountry}
+              autoComplete="off"
+              className="max-w-4xl md:flex-1"
+            >
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search for a country by its name"
+                required
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                className="py-3 px-4 text-gray-800
+               placeholder-gray-600 w-full shadow rounded outline-none
+               dark:text-gray-400 dark:placeholder-gray-400
+               dark:bg-gray-800 dark:focus:bg-gray-800 transition-all duration-200"
+              />
+            </form>
+            <form onSubmit={handleFilterByRegion}>
+              <select
+                name="filter-by-region"
+                id="filter-by-region"
+                value={regions.name}
+                onChange={(e) => filterByRegion(e.target.value)}
+                className="w-52 py-3 px-4 outline-none shadow rounded
+                 text-gray-600 dark:text-gray-400
+                 dark:bg-gray-800 dark:focus:bg-gray-700"
+              >
+                {regions.map((region, index) => (
+                  <option key={index} value={region.name}>
+                    {region.name}
+                  </option>
+                ))}
+              </select>
+            </form>
+          </div>
+          <div
+            className="grid cols-1 gap-8 md:grid-cols-2
+          lg:grid-cols-3 2xl:grid-cols-4"
+          >
+            {countries.map((country) => (
+              <Article key={country.name.common} {...country} />
+            ))}
           </div>
         </section>
       )}
